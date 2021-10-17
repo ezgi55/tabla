f9fc83fae842df82354d139a5ee5629014008f50
Erto <erto@mbp-MacBook-Pro.local>
2021-10-17 13:09:50 +0300

    erto

---
+++ tabla-api/infrastructure/database-up.sql
@@ -0,0 +1,54 @@
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '';
+
+CREATE DATABASE /* !32312 IF NOT EXISTS*/ "TABLAEvents_db" DEFAULT CHARSET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_cy
+
+USE 'Tabla_Events_db';
+
+# DROP TABLE users;
+# DROP TABLE events;
+# DROP TABLE cities;
+# DROP TABLE venues;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+user_id int AUTO INCREMENT PRIMARY KEY,
+first_name VARCHÄR (50) NOT NULL,
+last_name VARCHAR (50) NOT NULL,
+email VARCHAR (50) UNIQUE NOT NULL,
+password VARCHAR (500) NOT NULL,
+city VARCHAR(20),
+phone_number VARCHAR (20) NOT NULL
+);
+
+DROP TABLE IF EXISTS events;
+CREATE TABLE events(
+event_id int AUTO INCREMENT PRIMARY KEY,
+event_name VARCHAR (200) NOT NULL,
+event_artist VARCHAR (50) NOT NULL,
+event_date DATE NOT NULL,
+event_city VARCHAR (50) NOT NULL,
+event_description VARCHAR(200),
+address VARCHAR(50),
+image VARCHAR(250)
+);
+
+DROP TABLE IF EXISTS cities;
+CREATE TABLE cities (
+    city_name VARCHAR (5) NOT NULL,
+    city_venues VARCHAR (50) NOT NULL,
+    adress VARCHAR (50),
+    image VARCHAR (250)
+);
+DROP TABLE IF EXISTS venues;
+CREATE TABLE venues (
+venue_id int AUTO INCREMENT PRIMARY KEY,
+venue_name VARCHAR (50) NOT NULL,
+venue_type VARCHAR (10) NOT NULL,
+phone_number VARCHAR (50) NOT NULL,
+adress VARCHAR (50),
+image VARCHAR (250)
+);
---
+++ tabla-api/infrastructure/db-seed.sql
@@ -0,0 +1,39 @@
+/* Initialize DB with some seed data */
+USE 'Tabla_Events_db';
+
+INSERT INTO users (user_id, first_name, last_name, email, password, city, phone_number)
+VALUES ("1","Erto", "Coskuner", "erto_coskuner@hotmail.com", SHA512(CONCAT("12345", "PASSWORD") 224), "Lefkosa", "05338708798");
+INSERT INTO users (user_id, first_name, last_name, email, password, city, phone_number)
+VALUES ("2","Erhan", "Ersoy", "erhan0@hotmail.com", SHA512(CONCAT("12345", "PASSWORD") 224), "Lefkosa", "05338402304");
+INSERT INTO users (user_id, first_name, last_name, email, password, city, phone_number)
+VALUES ("3","Ezgi", "Akdag", "ezgiakdag@hotmail.com", SHA512(CONCAT("12345", "PASSWORD") 224), "Lefkosa", "05338837590");
+INSERT INTO users (user_id, first_name, last_name, email, password, city, phone_number)
+VALUES ("4","Emre", "Ertorun", "emreertorun@hotmail.com", SHA512(CONCAT("12345", "PASSWORD") 224), "Girne", "05428545292");
+INSERT INTO users (user_id, first_name, last_name, email, password, city, phone_number)
+VALUES ("5","Cem", "Gokdel", "cemgokdel@gmail.com", SHA512(CONCAT("12345", "PASSWORD") 224), "Girne", "05338219696");
+
+INSERT INTO events (event_name, event_artist, event_date, event_city, event_description, adress,)
+VALUES ("Halk Dansları" "Dans Ekibi" "25-09-2021" "Lefkosa" "Kültürel Buluşma" "bandabulya");
+INSERT INTO events (event_id, event_name, event_artist, event_date, event_city, event_description, adress,)
+VALUES ("EDM" "ReverseImpact" "1-10-2021" "Lefkosa" "Elektronik Dans Muzik" "Imagine");
+INSERT INTO events (event_id, event_name, event_artist, event_date, event_city, event_description, adress,)
+VALUES ("Canlı Muzik" "rast" "10-11-2021" "girne" "konser" "Girne Liman");
+
+INSERT INTO cities (city_name, city_venues, adress)
+VALUES ("Lefkosa","papa" "Surlar ici");
+INSERT INTO cities (city_name, city_venues, adress)
+VALUES ("Girne","Boat Cafe" "Liman");
+INSERT INTO cities (city_name, city_venues, adress)
+VALUES ("Magusa","Cafe no8" "Nadir Sk");
+INSERT INTO cities (city_name, city_venues, adress)
+VALUES ("Guzelyurt","pizzavira" "merkez");
+
+INSERT INTO venues (venue_name, venue_type, phone_number, address)
+VALUES ("papa" "bar" "05338882322" "surlar ici");
+INSERT INTO venues (venue_name, venue_type, phone_number, address)
+VALUES ("ezic" "restorant" "05338888888" "girne liman");
+INSERT INTO venues (venue_name, venue_type, phone_number, address)
+VALUES ("cafe no8" "cafe" "05338211212" "Nadir Sk");
+INSERT INTO venues (venue_name, venue_type, phone_number, address)
+VALUES ("Pizzavira" "resorant" "05338334433" "Merkez");
+
