424c67a300015214afd71025b9d2fbb233279d0e
Erto <erto@mbp-MacBook-Pro.local>
2021-10-22 15:46:23 +0300

    database düzenlemesi

--- tabla-api/infrastructure/db-seed.sql
+++ tabla-api/infrastructure/db-seed.sql
@@ -1,38 +1,41 @@
 /* Initialize DB with some seed data */
 USE `tabladb`;
 
-INSERT INTO users (first_name, last_name, email, password, city, phone_number)
-VALUES ("Erto", "Coskuner", "erto_coskuner@hotmail.com", "12345", "Lefkosa", "05338708798");
-INSERT INTO users (first_name, last_name, email, password, city, phone_number)
-VALUES ("Erhan", "Ersoy", "erhan0@hotmail.com", "12345", "Lefkosa", "05338402304");
-INSERT INTO users (first_name, last_name, email, password, city, phone_number)
-VALUES ("Ezgi", "Akdag", "ezgiakdag@hotmail.com", "12345", "Lefkosa", "05338837590");
-INSERT INTO users (first_name, last_name, email, password, city, phone_number)
-VALUES ("Emre", "Ertorun", "emreertorun@hotmail.com", "12345", "Girne", "05428545292");
-INSERT INTO users (first_name, last_name, email, password, city, phone_number)
-VALUES ("Cem", "Gokdel", "cemgokdel@gmail.com", "12345", "Girne", "05338219696");
+INSERT INTO user_roles (user_type)
+VALUES ("admin");
+INSERT INTO user_roles (user_type)
+VALUES ("customer");
+INSERT INTO user_roles (user_type)
+VALUES ("host");
 
-INSERT INTO events (event_name, event_artist, event_date, event_city, event_description, address)
-VALUES ("Halk Dansları", "Dans Ekibi", "25-09-2021", "Lefkosa", "Kültürel Buluşma", "bandabulya");
-INSERT INTO events (event_name, event_artist, event_date, event_city, event_description, address)
-VALUES ("EDM", "ReverseImpact", "1-10-2021", "Lefkosa", "Elektronik Dans Muzik", "Imagine");
-INSERT INTO events (event_name, event_artist, event_date, event_city, event_description, address)
-VALUES ("Canlı Muzik", "rast", "10-11-2021", "girne", "konser", "Girne Liman");
+INSERT INTO users (first_name, last_name, email, password, city, phone_number, user_role_id)
+VALUES ("Erto", "Coskuner", "erto_coskuner@hotmail.com", "12345", "Lefkosa", "05338708798", "1");
+INSERT INTO users (first_name, last_name, email, password, city, phone_number, user_role_id)
+VALUES ("Erhan", "Ersoy", "erhan0@hotmail.com", "12345", "Lefkosa", "05338402304", "1");
+INSERT INTO users (first_name, last_name, email, password, city, phone_number, user_role_id)
+VALUES ("Ezgi", "Akdag", "ezgi.e.akdag@gmail.com", "12345", "Lefkosa", "05338837590", "1");
+INSERT INTO users (first_name, last_name, email, password, city, phone_number, user_role_id)
+VALUES ("Emre", "Ertorun", "emreertorun@hotmail.com", "12345", "Girne", "05428545292", "1");
+INSERT INTO users (first_name, last_name, email, password, city, phone_number, user_role_id)
+VALUES ("Ahmet", "Akınsql", "gabi1@gmail.com", "12345", "Lefkosa", "05338214396", "2");
+INSERT INTO users (first_name, last_name, email, password, city, phone_number, user_role_id)
+VALUES ("muzaffer", "muzmuz", "muzmuz@gmail.com", "12345", "Girne", "05338219669", "2");
+INSERT INTO users (first_name, last_name, email, password, city, phone_number, user_role_id)
+VALUES ("Kopus", "Akdag", "havhav@gmail.com", "12345", "Lefkosa", "05338219123", "3");
 
-INSERT INTO cities (city_name, city_venues, address)
-VALUES ("Lefkosa", "papa", "Surlar ici");
-INSERT INTO cities (city_name, city_venues, address)
-VALUES ("Girne", "Boat Cafe", "Liman");
-INSERT INTO cities (city_name, city_venues, address)
-VALUES ("Magusa", "Cafe no8", "Nadir Sk");
-INSERT INTO cities (city_name, city_venues, address)
-VALUES ("Guzelyurt", "pizzavira", "merkez");
+INSERT INTO events (name, artist, date, description)
+VALUES ("Halk Dansları", "Dans Ekibi", "2021.09.25", "Kültürel Buluşma");
+INSERT INTO events (name, artist, date, description)
+VALUES ("EDM", "ReverseImpact", "2021.10.01", "Elektronik Dans Muzik";
+INSERT INTO events (name, artist, date, description)
+VALUES ("Canlı Muzik", "rast", "2021.11.10", "konser");
 
-INSERT INTO venues (venue_name, venue_type, phone_number, address)
-VALUES ("papa", "bar", "05338882322", "surlar ici");
-INSERT INTO venues (venue_name, venue_type, phone_number, address)
-VALUES ("ezic", "restorant", "05338888888", "girne liman");
-INSERT INTO venues (venue_name, venue_type, phone_number, address)
-VALUES ("cafe no8", "cafe", "05338211212", "Nadir Sk");
-INSERT INTO venues (venue_name, venue_type, phone_number, address)
-VALUES ("Pizzavira", "resorant", "05338334433", "Merkez");
+
+INSERT INTO venues (name, type, phone_number, address, image, city, user_id)
+VALUES ("papa", "bar", "05338882322", "surlar ici", "Lefkosa");
+INSERT INTO venues (name, type, phone_number, address, image, city, user_id)
+VALUES ("ezic", "restorant", "05338888888", "girne liman", "Girne");
+INSERT INTO venues (name, type, phone_number, address, image, city, user_id)
+VALUES ("cafe no8", "cafe", "05338211212", "Nadir Sk", "Magusa");
+INSERT INTO venues (name, type, phone_number, address, image, city, user_id)
+VALUES ("Pizzavira", "resorant", "05338334433", "Merkez", "Guzelyurt");
